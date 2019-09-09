d3d442a6896eae162aa600cbab82c8c45375754d
Ismet Ozkan <aysad@beyster1620p42.engin.umich.edu>
2019-09-09 15:32:58 -0400

    done

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+from random import randint
+
+def main():
+  a = randint(0,100)
+  b = randint(0,100)
+  print("Ismet Ozkan")
+  print(a)
+  print(b)
+  print("Sum =",a + b)
+  print("Average =",(a + b)/2)  
+
+if __name__ == "__main__":
+  main()
